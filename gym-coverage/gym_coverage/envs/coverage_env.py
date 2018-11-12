746fbd079485715896639e35fe09d1cddaa99140
cjg429 <jaegu.choy@cpslab.snu.ac.kr>
2018-11-12 16:56:49 +0900

    Change a model

--- gym-coverage/gym_coverage/envs/coverage_env.py
+++ gym-coverage/gym_coverage/envs/coverage_env.py
@@ -46,9 +46,10 @@ class CoverageEnv(gym.Env):
 
     def __init__(self):
 
+        self.dim = 6
         self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Tuple([spaces.Discrete(100), spaces.Box(low=-1.0, high=1.0, shape=(10,10), dtype=np.float32)])
-
+        #self.observation_space = spaces.Tuple([spaces.Discrete(100), spaces.Box(low=-1.0, high=1.0, shape=(10,10), dtype=np.float32)])
+        self.observation_space = spaces.Tuple([spaces.Discrete(self.dim*self.dim), spaces.Box(low=-1.0, high=1.0, shape=(self.dim,self.dim))])
         self.seed()
         self.viewer = None
         #self.reset()
@@ -61,11 +62,13 @@ class CoverageEnv(gym.Env):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
         state = self.state
         pos, map_curr = state
-        x_pos = pos // 10
-        y_pos = pos % 10
+        #x_pos = pos // 10
+        #y_pos = pos % 10
+        x_pos = pos // self.dim
+        y_pos = pos % self.dim
         col = False
         map_next = map_curr
-        
+        '''
         if action == 0:
             x_pos = x_pos - 1
             if x_pos < 0:
@@ -86,9 +89,31 @@ class CoverageEnv(gym.Env):
             if y_pos < 0:
                 col = True
                 y_pos = 0
-            
+        '''       
+        if action == 0:
+            x_pos = x_pos - 1
+            if x_pos < 0:
+                col = True
+                x_pos = 0
+        elif action == 1:
+            y_pos = y_pos + 1
+            if y_pos > self.dim - 1:
+                col = True
+                y_pos = self.dim - 1
+        elif action == 2:
+            x_pos = x_pos + 1
+            if x_pos > self.dim - 1:
+                col = True
+                x_pos = self.dim - 1
+        else:
+            y_pos = y_pos -1
+            if y_pos < 0:
+                col = True
+                y_pos = 0
+                
         done = col
-        pos = 10*x_pos + y_pos
+        #pos = 10*x_pos + y_pos
+        pos = self.dim*x_pos + y_pos
         map_next = map_next - 0.01       
         i, j = np.where(map_next == -1)
         np.clip(map_next, 0, 1, out=map_next)
@@ -104,18 +129,24 @@ class CoverageEnv(gym.Env):
         return self.state, reward, done, {}
 
     def reset(self):
-        pos = random.randint(0, 99)
+        #pos = random.randint(0, 99)
+        pos = random.randint(0, self.dim^2 - 1)
         #map_curr = self.np_random.uniform(low=-1.0, high=1.0, size=(10,10))
-        map_curr = self.np_random.uniform(low=-0.0, high=0.0, size=(10,10))
-        x_pos = pos // 10
-        y_pos = pos % 10
+        #map_curr = self.np_random.uniform(low=-0.0, high=0.0, size=(10,10))
+        map_curr = self.np_random.uniform(low=-0.0, high=0.0, size=(self.dim,self.dim))
+        #x_pos = pos // 10
+        #y_pos = pos % 10
+        x_pos = pos // self.dim
+        y_pos = pos % self.dim
         map_curr[x_pos, y_pos] = 1.0
         self.state = (pos, map_curr)
         return self.state
     
     def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 600
+        #screen_width = 600
+        #screen_height = 600
+        screen_width = 60 * self.dim
+        screen_height = 60 * self.dim
         robot_radius = 20
 
         if self.viewer is None:
@@ -123,35 +154,56 @@ class CoverageEnv(gym.Env):
             self.viewer = rendering.Viewer(screen_width, screen_height)
            
             self.cell_color = []
+            '''
             for x in range(0, 10):
                 for y in range(0, 10):
                     cell = rendering.FilledPolygon([(60*x,60*y), (60*(x+1),60*y), (60*(x+1),60*(y+1)), (60*x,60*(y+1))])
                     self.cell_color.append(cell.attrs[0])
-                    self.viewer.add_geom(cell)  
-                    
+                    self.viewer.add_geom(cell) 
+            '''        
+            for x in range(0, self.dim):
+                for y in range(0, self.dim):
+                    cell = rendering.FilledPolygon([(60*x,60*y), (60*(x+1),60*y), (60*(x+1),60*(y+1)), (60*x,60*(y+1))])
+                    self.cell_color.append(cell.attrs[0])
+                    self.viewer.add_geom(cell)         
             robot = rendering.make_circle(robot_radius)
             robot.set_color(1.0, 0.0, 0.0)
             self.robotrans = rendering.Transform()
             robot.add_attr(self.robotrans)
             self.viewer.add_geom(robot)
-            
+            '''
             for i in range(1, 10):
                 line = rendering.Line((60*i, 0), (60*i, 600))
                 self.viewer.add_geom(line) 
             for i in range(1, 10):
                 line = rendering.Line((0, 60*i), (600, 60*i))
+                self.viewer.add_geom(line)
+            '''    
+            for i in range(1, self.dim):
+                line = rendering.Line((60*i, 0), (60*i, 60*self.dim))
+                self.viewer.add_geom(line) 
+            for i in range(1, self.dim):
+                line = rendering.Line((0, 60*i), (60*self.dim, 60*i))
                 self.viewer.add_geom(line) 
 
         pos = self.state[0]
-        x_pos = 30 + 60 * (pos // 10)
-        y_pos = 30 + 60 * (pos % 10)
+        #x_pos = 30 + 60 * (pos // 10)
+        #y_pos = 30 + 60 * (pos % 10)
+        x_pos = 30 + 60 * (pos // self.dim)
+        y_pos = 30 + 60 * (pos % self.dim)
         self.robotrans.set_translation(x_pos, y_pos)
         
         map_curr = self.state[1]    
+        '''
         for x in range(0, 10):
             for y in range(0, 10):
                 transparency = (map_curr[x, y] + 1) / 2
                 self.cell_color[10*x+y].vec4 = ((transparency, transparency, transparency, 1))
+        '''
+        for x in range(0, self.dim):
+            for y in range(0, self.dim):
+                transparency = (map_curr[x, y] + 1) / 2
+                self.cell_color[self.dim*x+y].vec4 = ((transparency, transparency, transparency, 1))
                 #self.cell_color[x*10+y].vec4 = ((0.5, 0.5, 0.5, 1))
         #self.cell_color[pos].vec4 = ((1.0, 1.0, 1.0, 1))
         #self.cartrans.set_rotation(math.cos(3 * pos))
